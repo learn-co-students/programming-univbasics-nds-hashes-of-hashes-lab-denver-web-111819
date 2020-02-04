3e421aa6b32086cfddaf1736bd2510822d4329f5
Josh Couper <joshcouper@gmail.com>
2020-02-04 00:25:14 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,26 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+  
+   hasher = {
+  :sub_category => {
+    :sub_category => {
+      :sub_category => {
+        :label => "Order",
+        :sub_category => {
+          :sub_category => {
+            :label => "Genus",
+            :sub_category => {
+             :label => "Species"
+            }
+          }
+        }
+      }
+      
+      }
+    }
+  }
+ 
 
+  
 end
